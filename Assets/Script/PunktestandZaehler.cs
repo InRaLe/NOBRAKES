cfeb4919bf02c035399e261b02f7dc7e2d0f9404
LEr <leonard.roemer@gmx.de>
2019-01-19 15:13:51 +0100

    Punktezähler hinzugefügt

---
+++ Assets/Script/PunktestandZaehler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PunktestandZaehler : MonoBehaviour
+{
+    public Transform player;
+    public Text punktestandText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        punktestandText.text = player.position.z.ToString("0") + " Meter";
+    }
+}
